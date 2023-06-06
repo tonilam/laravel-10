21db977c714ba80a9cd893fb0c4c390c8e21766c
tonilam <toni_lam@hotmail.com>
2023-06-06 13:48:21 +1000

    Add basic models

---
+++ app/Models/Company.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    public function employees(): HasMany {
+        return $this->hasMany(\App\Models\Employee::class);
+    }
+}
